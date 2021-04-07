28938a8f20bbd9ab36964591720c1274be9cd81b
yashar6846 <yasharnajafi6846@gmail.com>
2021-04-07 13:07:56 +0300

    Build new reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import Reduser from './Commonents/Reducer'
+import Reduser from './Commonents/Reducer';
+import Reducerone from './Commonents/Reducer/Reducerone';
 function App() {
   return (
     <div className="App">
-     
+     <Reducerone />
     </div>
   );
 }
--- src/Commonents/Reducer/index.js
+++ src/Commonents/Reducer/index.js
@@ -1,1 +1,2 @@
-export {default} from './Reducer'
+export {default} from './Reducer';
+export {default} from './Reducerone'
